784bd511d4addd3d3324431e73cc392a6cb8519c
fbartolome <fbartolome18@gmail.com>
2016-10-23 16:53:07 -0300

    intentando empezar con el parser.

--- src/main/java/ar/edu/itba/pdc/natto/protocol/xmpp/XmppParser.java
+++ src/main/java/ar/edu/itba/pdc/natto/protocol/xmpp/XmppParser.java
@@ -15,7 +15,7 @@ import java.util.Queue;
 
 public class XmppParser implements Parser<String> {
 
-
+/*
     private enum XmppState {
         STREAM("stream");
 
@@ -24,28 +24,34 @@ public class XmppParser implements Parser<String> {
         XmppState(String value) {
             this.value = value;
         }
-    }
+    }*/
 
     private Queue<StringBuilder> parsed = new LinkedList<>();
     private StringBuilder current;
-    private Deque<XmppState> stateStack = new LinkedList<>();
+   // private Deque<XmppState> stateStack = new LinkedList<>();
+    private Queue<String> tags = new LinkedList<>();
+    private String currentTag = "";
+    boolean insideTag = false;
+    boolean unfinishedTag = false;
 
     @Override
     public String fromByteBuffer(ByteBuffer buffer) {
         //appendBuffer(buffer);
         String bufferString = bufferToString(buffer);
-        for (int i = 0; i < bufferString.length(); i++) {
+
+        /*for (int i = 0; i < bufferString.length(); i++) {
             if (bufferString.charAt(i) == '<') {
-                if (bufferString.) {
+                if (true) {
 
                 }
 
             } else if (bufferString.charAt(i) == '>') {
 
             }
-        }
+        }*/
+        tagsToQueue(bufferString);
 
-        return null;
+        return tags.remove();
     }
 
     @Override
@@ -62,5 +68,49 @@ public class XmppParser implements Parser<String> {
     private String bufferToString(ByteBuffer buffer) {
         return new String(buffer.array(), buffer.position(), buffer.limit(),
                 Charset.forName("UTF-8"));
+    }
+
+    /**
+     * Recognizes tags within the string and inserts them into the tags queue.
+     *
+     * @param bufferString
+     */
+    void tagsToQueue(String bufferString){
+
+        for(int i = 0; i < bufferString.length(); i++){
+
+            if(!insideTag){
+
+                if(bufferString.charAt(i) == '<'){
+
+                    //info que no pertenece a un tag. Ej: <etiqueta>Soy una etiqueta</etiqueta>
+                    if(!currentTag.equals("")){
+                        tags.add(currentTag);
+                        currentTag = "";
+                    }
+
+                    currentTag += bufferString.charAt(i);
+                    insideTag = true;
+                    unfinishedTag = true;
+                }else{
+                    currentTag += bufferString.charAt(i);
+                }
+
+            }else{
+
+                if(bufferString.charAt(i) == '>'){
+                    currentTag += bufferString.charAt(i);
+                    insideTag = false;
+                    unfinishedTag = false;
+                    tags.add(currentTag);
+                    currentTag = "";
+                }else{
+                    currentTag += bufferString.charAt(i);
+                }
+
+            }
+
+        }
+
     }
 }
