6f57ffbc44e7ba295973f53803eb081fc6b73665
natalia navas <natinavas@gmail.com>
2016-10-26 19:31:06 -0300

    fixed errors in parser

--- src/main/java/ar/edu/itba/pdc/natto/protocol/xmpp/XmppParser.java
+++ src/main/java/ar/edu/itba/pdc/natto/protocol/xmpp/XmppParser.java
@@ -25,6 +25,8 @@ public class XmppParser implements Parser<Tag> {
     private AsyncXMLStreamReader<AsyncByteBufferFeeder> parser = null;
     private Queue<ByteBuffer> buffers = new LinkedList<>();
     private Deque<Tag> tagDequeue = new LinkedList<>();
+    private int type; // TODO: El primer caso pq es 0?
+
 
     private boolean newTag = true;
 
@@ -56,6 +58,8 @@ public class XmppParser implements Parser<Tag> {
             return handleTooBig();
         }
 
+        buffer.limit(buffer.limit()-1);
+
         // Agrego el buffer para devolverlo en el caso de que no lo modifique
         buffers.add(buffer);
 
@@ -70,103 +74,122 @@ public class XmppParser implements Parser<Tag> {
         }
 
         // Aca empieza la etapa de parseo
-        int type = 0; // TODO: El primer caso pq es 0?
-
         Tag stanza = null;
 
-        do {
-            Tag tag = null;
-            System.out.println("El tamaño de la pila de tags es: " + tagDequeue.size());
 
-            switch (type) {
+        try {
+            while(parser.hasNext()){
+                Tag tag = null;
 
-                case AsyncXMLStreamReader.START_DOCUMENT:
-                    System.out.println("start document");
-                    break;
+                try {
+                    type = parser.next();
+                } catch (com.fasterxml.aalto.WFCException e) {
+                    // TODO: Que hacer?
+                    return handleWrongFormat(tag);
+                } catch (XMLStreamException e) {
+                    // TODO: Que hacer?
+                    e.printStackTrace();
+                    return null; // TODO: Que retornar?
+                }
 
-                case AsyncXMLStreamReader.START_ELEMENT:
-                    if (tagDequeue.size() == 0) {
-                        if (parser.getName().getLocalPart().toString().equals("iq")) {
-                            tag = new Iq();
-                        } else if (parser.getName().getLocalPart().toString().equals("presence")) {
-                            tag = new Presence();
-                        } else if (parser.getName().getLocalPart().toString().equals("message")) {
-                            tag = new Message();
-                        } else if (parser.getName().getLocalPart().toString().equals("stream")) {
-                            tag = new Stream();
-                            addAttributes(tag);
-                            tag.setPrefix(parser.getPrefix());
-                            tag.addNamespace(parser.getName().getNamespaceURI());
+                switch (type) {
 
-                            newTag = true;
-                            // TODO: Falta cerrar bien el parser
-                            return tag;
+                    case AsyncXMLStreamReader.START_DOCUMENT:
+                        System.out.println("start document");
+                        break;
+
+                    case AsyncXMLStreamReader.START_ELEMENT:
+                        System.out.println("start element " + parser.getName());
+                        if (tagDequeue.size() == 0) {
+                            if (parser.getName().getLocalPart().toString().equals("iq")) {
+                                tag = new Iq();
+                            } else if (parser.getName().getLocalPart().toString().equals("presence")) {
+                                tag = new Presence();
+                            } else if (parser.getName().getLocalPart().toString().equals("message")) {
+                                tag = new Message();
+                            } else if (parser.getName().getLocalPart().toString().equals("stream")) {
+                                tag = new Stream();
+                                addAttributes(tag);
+                                tag.setPrefix(parser.getPrefix());
+                                tag.addNamespace(parser.getName().getNamespaceURI());
+
+                                newTag = true;
+
+                                try {
+                                    parser.close();
+                                } catch (XMLStreamException e) {
+                                    e.printStackTrace();
+                                }
+                                // TODO: Falta cerrar bien el parser
+                                return tag;
+
+                            } else {
+                                System.out.println("entro aca como deberia");
+                                tag = new Tag(parser.getName().getLocalPart(), false);
+                            }
 
                         } else {
-                            tag = new Tag(parser.getName().getLocalPart(), false);
+                            boolean empty = true;
+
+                            try {
+                                empty = parser.isEmptyElement();
+                            } catch (XMLStreamException e) {
+                                // TODO: Que hacer?
+                                e.printStackTrace();
+                            }
+
+                            tag = new Tag(parser.getName().getLocalPart(), empty);
+                            tagDequeue.peek().addTag(tag);
+
                         }
 
-                    } else {
-                        boolean empty = true;
+                        addAttributes(tag);
+                        tag.setPrefix(parser.getPrefix());
+                        tag.addNamespace(parser.getName().getNamespaceURI());
+                        System.out.println(" tag " + tag);
+                        tagDequeue.push(tag);
+                        break;
 
-                        try {
-                            empty = parser.isEmptyElement();
-                        } catch (XMLStreamException e) {
-                            // TODO: Que hacer?
-                            e.printStackTrace();
+                    case AsyncXMLStreamReader.CHARACTERS:
+                        tagDequeue.peek().setValue(parser.getText());
+                        System.out.println("estos son mis chars " + parser.getText());
+                        break;
+
+                    case AsyncXMLStreamReader.END_ELEMENT:
+                        if (tagDequeue.size() == 1) {
+                            stanza = tagDequeue.poll();
+                        }else{
+                            tagDequeue.poll();
                         }
+                        System.out.println("end element " + parser.getName());
+                        break;
 
-                        tag = new Tag(parser.getName().getLocalPart(), empty);
-                        tagDequeue.peek().addTag(tag);
+                    case AsyncXMLStreamReader.EVENT_INCOMPLETE:
+                       //System.out.println("incomplete name " + parser.getName());
+                        if(!tagDequeue.isEmpty()){
+                            System.out.println("taaaag " + tagDequeue.peek());
+                        }
+                        System.out.println("incomplete");
+                        return null;
 
-                    }
-
-                    addAttributes(tag);
-                    tag.setPrefix(parser.getPrefix());
-                    tag.addNamespace(parser.getName().getNamespaceURI());
-                    tagDequeue.push(tag);
-                    break;
-
-                case AsyncXMLStreamReader.CHARACTERS:
-                    tag = tagDequeue.poll();
-                    tag.setValue(parser.getText());
-                    tagDequeue.push(tag);
-                    break;
-
-                case AsyncXMLStreamReader.END_ELEMENT:
-                    if (tagDequeue.size() == 1) {
-                        stanza = tagDequeue.poll();
-                    }
-
-                    tagDequeue.poll();
-                    break;
-
-                case AsyncXMLStreamReader.EVENT_INCOMPLETE:
-                    System.out.println("incomplete");
-
-                    return stanza; // TODO: Aca tiene que retornar null
-
-                default:
-                    break;
+                    default:
+                        break;
+                }
             }
-
-            try {
-                type = parser.next();
-            } catch (com.fasterxml.aalto.WFCException e) {
-                // TODO: Que hacer?
-                return handleWrongFormat(tag);
-            } catch (XMLStreamException e) {
-                // TODO: Que hacer?
-                e.printStackTrace();
-                return null; // TODO: Que retornar?
-            }
-
-        } while (type != AsyncXMLStreamReader.END_DOCUMENT);
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        }
 
         System.out.println(stanza);
 
         newTag = true;
-        // TODO: Falta cerrar bien el parser
+
+        parser.getInputFeeder().endOfInput();
+        try {
+            parser.close();
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        }
 
         return stanza;
     }
