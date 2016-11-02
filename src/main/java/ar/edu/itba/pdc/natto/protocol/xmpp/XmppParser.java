08a5042f3b52375ae13036c44874fbde5ed9e03e
natalia navas <natinavas@gmail.com>
2016-11-02 16:46:58 -0300

    Tercer parser... la tercera es la vencida

--- src/main/java/ar/edu/itba/pdc/natto/protocol/xmpp/XmppParser.java
+++ src/main/java/ar/edu/itba/pdc/natto/protocol/xmpp/XmppParser.java
@@ -9,7 +9,6 @@ import com.fasterxml.aalto.stax.InputFactoryImpl;
 
 import javax.xml.stream.XMLStreamException;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -19,13 +18,18 @@ import static com.google.common.base.Preconditions.checkState;
 
 // TODO: Fijarse de siempre cerrar bien el parser anterior!
 public class XmppParser implements Parser<Tag> {
+
+    private String message = "<iqqq:iq xmlns:iqqq=\"holaaaaaa\"><hola></hola></iqqq:iq>";
+    private ByteBuffer buffer2 = ByteBuffer.wrap(message.getBytes());
+
     private final static int BUFFER_MAX_SIZE = 10000;
+
     private AsyncXMLInputFactory inputF = new InputFactoryImpl();
     private AsyncXMLStreamReader<AsyncByteBufferFeeder> parser = null;
     private Queue<ByteBuffer> buffers = new LinkedList<>();
     private Deque<Tag> tagDequeue = new LinkedList<>();
     private int type; // TODO: El primer caso pq es 0?
-    private boolean completeTag = false;
+
 
     private boolean newTag = true;
 
@@ -43,9 +47,6 @@ public class XmppParser implements Parser<Tag> {
 
     @Override
     public Tag fromByteBuffer(ByteBuffer buffer) {
-        System.out.println("\nMe llega: " + new String(buffer.array()));
-
-        System.out.println("\n\n\n\n");
         if (buffer == null) {
             return null;
         }
@@ -60,54 +61,31 @@ public class XmppParser implements Parser<Tag> {
             return handleTooBig();
         }
 
-        if (parser.getInputFeeder().needMoreInput()) {
-            System.out.println("Necesito mas cosas para el parser");
-            try {
-                parser.getInputFeeder().feedInput(buffer);
+        // Agrego el buffer para devolverlo en el caso de que no lo modifique
+        buffers.add(buffer);
 
-            } catch (XMLStreamException e) {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-                newTag = true;
-                return null; // TODO: ? Wrongformat tag // Falta cerrar bien el parser //Forwardear o que hacer? ???
-            }
-        } else {
-            System.out.println("el parser tiene cosas, ver que hacer con buffer que entra"); //TODO
+        try {
+            parser.getInputFeeder().feedInput(buffer);
+        } catch (XMLStreamException e) {
+            // TODO: Forwardear o que hacer? ???
+            e.printStackTrace();
+            newTag = true;
+            // TODO: Falta cerrar bien el parser
+            return null; // TODO: ? Wrongformat tag
         }
-
 
         // Aca empieza la etapa de parseo
         Tag message = null;
 
-        int position = 0;
         try {
             message = parse();
-            System.out.println("Llegue 1");
-            position = (int) parser.getLocationInfo().getEndingByteOffset();
-            System.out.println("Llegue 2");
-
         } catch (XMLStreamException e) {
-            System.out.println(e.getMessage());
             System.out.println("Mensaje mal formado"); // TODO: Loguear
             message = handleWrongFormat();
         }
 
-        while (position<buffer.limit() && Character.isWhitespace(buffer.get(position))) {
-            position++;
-        }
-
-        ByteBuffer auxBuffer = buffer.slice();
-        buffer.position(position + buffer.position());
-        auxBuffer.limit(position);
-        System.out.println("\nAUX: " + new String(auxBuffer.array(), auxBuffer.position(), auxBuffer.limit()));
-        System.out.println(auxBuffer);
-        System.out.println("\nNOAUX: " + new String(buffer.array(), buffer.position(), buffer.limit()));
-        System.out.println(buffer);
-
-        // Agrego el buffer para devolverlo en el caso de que no lo modifique
-        buffers.add(auxBuffer);
-
         if (message != null) {
+            System.out.println(message);
 
             newTag = true;
 
@@ -120,19 +98,20 @@ public class XmppParser implements Parser<Tag> {
             checkState(tagDequeue.isEmpty());
         }
 
-        completeTag = false;
-
         return message;
     }
 
     private Tag parse() throws XMLStreamException {
         Tag ret = null;
 
-
         while (parser.hasNext()) {
             Tag tag;
 
             switch (parser.next()) {
+                case AsyncXMLStreamReader.START_DOCUMENT:
+                    System.out.println("start document");
+                    break;
+
                 case AsyncXMLStreamReader.START_ELEMENT:
                     System.out.println("start element " + parser.getName());
 
@@ -144,74 +123,51 @@ public class XmppParser implements Parser<Tag> {
                         } else if (name.equals("presence")) {
                             tag = new Presence();
                         } else if (name.equals("message")) {
-                            completeTag = true;
                             tag = new Message();
                         } else if (name.equals("stream")) {
                             tag = new Stream();
-
-                        } else if (name.equals("auth")) {
-                            completeTag = true;
-                            tag = new Tag("auth", false); //TODO: crear un objeto auth
                         } else {
                             tag = new Tag(name, false);
                         }
-                        addAttributes(tag);
-                        tag.setPrefix(parser.getPrefix());
-                        tag.addNamespace(parser.getName().getNamespaceURI());
-
-                        if (parser.getPrefix().equals("stream")) {
-                            // addAttributes(tag);
-                            // tag.setPrefix(parser.getPrefix());
-                            // tag.addNamespace(parser.getName().getNamespaceURI());
-                            System.out.println("tamaño -> " + parser.getLocationInfo().getEndingByteOffset());
-                            parser.getInputFeeder().endOfInput();
-                            return tag;
-                        } else {
-                            tagDequeue.push(tag);
-                        }
-
                     } else {
-                        if (completeTag) {
-                            boolean empty = true;
+                        boolean empty = true;
 
-                            try {
-                                empty = parser.isEmptyElement();
-                            } catch (XMLStreamException e) {
-                                // TODO: Que hacer?
-                                e.printStackTrace();
-                                // TODO: Return?
-                            }
-
-                            tag = new Tag(parser.getName().getLocalPart(), empty);
-                            addAttributes(tag);
-                            tag.setPrefix(parser.getPrefix());
-                            tag.addNamespace(parser.getName().getNamespaceURI());
-                            tagDequeue.peek().addTag(tag);
-                            tagDequeue.push(tag);
-                        } else {
-                            tagDequeue.push(new Tag(parser.getName().getLocalPart(), false));
+                        try {
+                            empty = parser.isEmptyElement();
+                        } catch (XMLStreamException e) {
+                            // TODO: Que hacer?
+                            e.printStackTrace();
+                            // TODO: Return?
                         }
 
+                        tag = new Tag(parser.getName().getLocalPart(), empty);
+                        tagDequeue.peek().addTag(tag);
                     }
+
+                    addAttributes(tag);
+                    tag.setPrefix(parser.getPrefix());
+                    tag.addNamespace(parser.getName().getNamespaceURI());
+
+                    if (tag.getName().equals("stream")) {
+                        return tag;
+                    } else {
+                        tagDequeue.push(tag);
+                    }
+
                     break;
 
                 case AsyncXMLStreamReader.CHARACTERS:
                     System.out.println("Character: " + parser.getText());
 
-                    if (completeTag) {
-                        tagDequeue.peek().setValue(parser.getText());
-                    }
+                    tagDequeue.peek().setValue(parser.getText());
 
                     break;
 
                 case AsyncXMLStreamReader.END_ELEMENT:
                     System.out.println("End element: " + parser.getName());
-                    System.out.println("tama;o de la pila " + tagDequeue.size());
 
                     if (tagDequeue.size() == 1) {
                         parser.getInputFeeder().endOfInput();
-                        // System.out.println("CIERRO EL INPUT ACA EN EL END_ELEMENT");
-                        // newTag = true;
                         ret = tagDequeue.poll();
                     } else {
                         tagDequeue.poll();
@@ -220,7 +176,8 @@ public class XmppParser implements Parser<Tag> {
                     break;
 
                 case AsyncXMLStreamReader.EVENT_INCOMPLETE:
-                    System.out.println("Incomplete! En el stack tengo: " + tagDequeue.peek());
+                    System.out.println("Incomplete!");
+
                     return null;
 
                 default:
@@ -228,7 +185,7 @@ public class XmppParser implements Parser<Tag> {
                     break;
             }
         }
-        System.out.println("Tamaño" + parser.getLocationInfo().getEndingByteOffset());
+
         checkNotNull(ret);
 
         return ret;
@@ -241,18 +198,14 @@ public class XmppParser implements Parser<Tag> {
             while (!buffers.isEmpty()) {
                 ret.put(buffers.poll());
             }
-
             ret.flip();
-            System.out.println("Devuelvo: " + new String(ret.array(), ret.position(), ret.limit(),
-                    StandardCharsets.UTF_8));
             return ret;
         }
+
         while (!buffers.isEmpty()) {
             //TODO: hasta donde borrar?
             buffers.poll();
         }
-        System.out.println("aca estoy justo antes de devolver algo modificado");
-        System.out.println("Devulelvo: " + message);
         return ByteBuffer.wrap(message.toString().getBytes());
     }
 
