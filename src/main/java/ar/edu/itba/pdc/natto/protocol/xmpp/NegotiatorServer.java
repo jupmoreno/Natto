3b35e04b08538f445b7501cfb11a8abc0ee7abe1
natalia navas <natinavas@gmail.com>
2016-11-09 15:03:55 -0300

    negotiator server uses buffer again

--- src/main/java/ar/edu/itba/pdc/natto/protocol/xmpp/NegotiatorServer.java
+++ src/main/java/ar/edu/itba/pdc/natto/protocol/xmpp/NegotiatorServer.java
@@ -16,15 +16,13 @@ public class NegotiatorServer implements Negotiator {
     private AsyncXMLInputFactory inputF = new InputFactoryImpl();
     private AsyncXMLStreamReader<AsyncByteBufferFeeder> reader = inputF.createAsyncForByteBuffer();
 
-    private ByteBuffer retBuffer = ByteBuffer.allocate(1000000);
-    private StringBuilder sb = new StringBuilder();
+    private ByteBuffer retBuffer = ByteBuffer.allocate(100000);
 
     private boolean inMech = false;
     private boolean hasPlain = false;
     private boolean hasToWrite = false;
 
     private boolean verified = false;
-
     private String user64;
 
     @Override
@@ -37,16 +35,9 @@ public class NegotiatorServer implements Negotiator {
 
         VerificationState readResult = VerificationState.INCOMPLETE;
 
-        System.out.println("ENTRO AL HANDSHAKE Y EL BUFFER QUE ME ENTRA ES " + new String(readBuffer.array(), readBuffer.position(), readBuffer.limit()));
-
-        retBuffer = ByteBuffer.allocate(10000); //TODO SACAR
-
         if(reader.getInputFeeder().needMoreInput()){
             try {
-                System.out.println("feedeo al buffer");
-                System.out.println("EL BUFFER QUE FFEEEDEO ES "+ new String(readBuffer.array(), readBuffer.position(), readBuffer.limit()));
                 reader.getInputFeeder().feedInput(readBuffer);
-                retBuffer.clear();
             } catch (XMLStreamException e) {
                 return handleWrongFormat(connection);
             }
@@ -63,16 +54,16 @@ public class NegotiatorServer implements Negotiator {
 
             switch (readResult){
                 case FINISHED:
-                    System.out.println("TERMINO DE NEGOCIAR :)");
                     verified = true;
+                    retBuffer.clear();
                     //TODO: habilitar que el cliente pueda escribir en el servidor
-                    // connection.requestRead();  ???
                     return 1;
 
                 case IN_PROCESS:
                     if(hasToWrite){
                         connection.requestWrite(retBuffer);
-                        retBuffer = ByteBuffer.allocate(10000); //TODO SACAR DIUJ
+                        retBuffer.clear();
+                        hasToWrite = false;
                     }
 
                     break;
@@ -87,8 +78,6 @@ public class NegotiatorServer implements Negotiator {
                         retBuffer.clear();
                     }
             }
-
-
 
         }
 
@@ -109,23 +98,18 @@ public class NegotiatorServer implements Negotiator {
                     break;
 
                 case AsyncXMLStreamReader.START_ELEMENT:
-                    //     System.out.println("start element");
                     return handleStartElement();
 
                 case AsyncXMLStreamReader.CHARACTERS:
                     if(inMech && reader.getText().equals("PLAIN")){
                         hasPlain = true;
                     }
-                    //System.out.println("characters");
-                    //System.out.println("los characters que me llegan son " + reader.getText());
                     break;
 
                 case AsyncXMLStreamReader.END_ELEMENT:
-                    //System.out.println("end element");
                     return handleEndElement();
 
                 case AsyncXMLStreamReader.EVENT_INCOMPLETE:
-                    //System.out.println("incomplete");
                     return VerificationState.INCOMPLETE;
 
                 default:
@@ -133,14 +117,13 @@ public class NegotiatorServer implements Negotiator {
             }
         }
 
-        ///????
         return VerificationState.ERR;
 
     }
 
 
     private VerificationState handleEndElement(){
-        //   System.out.println("en el end element el nombre del tag es " + reader.getLocalName());
+
         if(reader.getPrefix().equals("stream") && reader.getLocalName().equals("features")){
             String ret = "<auth xmlns=\"urn:ietf:params:xml:ns:xmpp-sasl\" mechanism=\"PLAIN\">" + user64 + "</auth>";
             retBuffer = ByteBuffer.wrap(ret.getBytes());
@@ -151,27 +134,25 @@ public class NegotiatorServer implements Negotiator {
             inMech = false;
         }
 
-        if(reader.getLocalName().equals("success")){
-            //     System.out.println("ESTA ACA EN END ELEMENT");
+        if(reader.getLocalName().equals("success") && hasPlain){
+            return VerificationState.FINISHED;
         }
+
         //TODO pensar bien que resp:
         return VerificationState.IN_PROCESS;
 
     }
 
     private VerificationState handleStartElement(){
-        //      System.out.println("en en elemente l nombre del tag es " + reader.getLocalName());
 
-        if(reader.getLocalName().equals("success") && hasPlain){
-            //        System.out.println("ESTA ACA EN START ELEMENT");
-            return VerificationState.FINISHED;
-        }
         if(reader.getLocalName().equals("mechanism")){
             inMech = true;
         }
 
-        if(reader.getLocalName().equals("message") || reader.getLocalName().equals("iq") || reader.getLocalName().equals("presence"))
+        if(reader.getLocalName().equals("message") || reader.getLocalName().equals("iq") || reader.getLocalName().equals("presence")){
             return handleNotAuthorized();
+        }
+
 
         return VerificationState.IN_PROCESS;
 
@@ -180,6 +161,9 @@ public class NegotiatorServer implements Negotiator {
     public void setUser64(String user64) {
         this.user64 = user64;
     }
+
+
+    /**Error Handlers**/
 
     /**
      * RFC 4.9.3.1.  bad-format
